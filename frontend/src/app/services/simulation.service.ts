c233bf55b43f1bbacb31718aeb8f0f085190459a
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-08-27 11:16:24 +0000

    change apiUrlk to backend deployment

--- frontend/src/app/services/simulation.service.ts
+++ frontend/src/app/services/simulation.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SimulationService {
-  private apiUrl = 'http://localhost:3000/api/simulations';
+  private apiUrl = 'https://logidoo.onrender.com/api/simulations';
 
   constructor(private http: HttpClient) {}
 
