c869112ebb527e6a8cf09c85c944743b5041b596
FlaskSPoon <admin@fireshieldsec.com>
2025-09-17 01:47:03 -1200

    landing page

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-page.component.html',
-   styleUrl:'./landing-page.component.scss'
+     styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
   constructor(public authService: AuthService) {}
