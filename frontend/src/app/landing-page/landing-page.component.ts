6ec0621ec971b9648a4e07c5aa03952cf602b4d0
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-09-20 09:35:55 +0000

    seny's work for landing page

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatIconModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
