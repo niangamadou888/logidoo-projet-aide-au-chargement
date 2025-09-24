f4c082f26de3887935225c5b1058f67a0513ae74
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-09-24 21:34:53 +0000

    fix deployment frontend error

--- frontend/dist/front-end/browser/es/chunk-EWMLP7AX.js
+++
@@ -1,2 +0,0 @@
-function o(e){"@babel/helpers - typeof";return o=typeof Symbol=="function"&&typeof Symbol.iterator=="symbol"?function(t){return typeof t}:function(t){return t&&typeof Symbol=="function"&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t},o(e)}export{o as a};
-/**i18n:f9f9359da0ae120721110188cf40ae175ebd0866172b75df7a3253468841815f*/
